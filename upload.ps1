54c4d96d7708c5ebec97b987a7050ed500dee66f
Javier Castro <javiercastro@nexion.com.ar>
2024-03-03 10:11:07 -0300

    Refactor Get-MultipartBody

--- upload.ps1
+++ upload.ps1
@@ -1,30 +1,40 @@
-function Get-MultipartBody($boundary, $paths, $fields) {
-    $boundary = "`r`n--" + $boundary
-    $multipart = @($fields.Keys + $paths) | % {
-            $name = if ($paths.IndexOf($_) -ne -1) {'upload_files'} else {$_}
-            $contentDisposition = "Content-Disposition: form-data; name=`"$name`""
-            $contentType = ''
-            $data = ''
-            if ($name -eq 'upload_files') {
-                $filename = [IO.Path]::GetFileName($_)
-                $contentDisposition = $contentDisposition + "; filename=`"$filename`""
-                $contentType = "`r`n`Content-Type: application/x-msdownload"
-                $bytes = [IO.File]::ReadAllBytes($_)
-                $data = [Text.Encoding]::GetEncoding('ISO-8859-1').GetString($bytes)
-            } else {
-                $data = $fields.Item($_)
-            }
-            return $contentDisposition + $contentType + "`r`n`r`n" + $data
+function Get-MultipartBody($boundary, $files, $fields) {
+    $tempFile = [System.IO.Path]::GetTempFileName()
+    $UTF8woBOM = New-Object "System.Text.UTF8Encoding" -ArgumentList @($false)
+    # Campos
+    if ($fields.Keys.Count -gt 0) {
+        $sw = New-Object System.IO.StreamWriter($tempFile, $true, $UTF8woBOM)
+        foreach($field in $fields.Keys) {
+            $sw.Write("`r`n--$boundary`r`nContent-Disposition: form-data; name=`"$field`"`r`n`r`n$($fields.Item($field))")
         }
-    $middle = $multipart -join ($boundary + "`r`n")
-    return $boundary + "`r`n" + $middle + $boundary + "--"
+        $sw.Close()
+    }
+    # Archivos binarios
+    if ($files.Length -gt 0) {
+        $files | % {
+            $sw = New-Object System.IO.StreamWriter($tempFile, $true, $UTF8woBOM)
+            $fileName = [System.IO.Path]::GetFileName($_)
+            $sw.Write("`r`n--$boundary`r`nContent-Disposition: form-data;name=`"upload_files`";filename=`"$fileName`"`r`n`r`n")
+            $sw.Close()
+
+            $fs = New-Object System.IO.FileStream($tempFile, [System.IO.FileMode]::Append)
+            $bw = New-Object System.IO.BinaryWriter($fs)
+            $fileBinary = [System.IO.File]::ReadAllBytes($_)
+            $bw.Write($fileBinary)
+            $bw.Close()
+        }
+    }
+    $sw = New-Object System.IO.StreamWriter($tempFile, $true, $UTF8woBOM)
+    $sw.Write("`r`n--$boundary--`r`n")
+    $sw.Close()
+    $tempFile
 }
 
-$fields = @{}
+$fields = @{name="Javier"; lastname="Castro"}
 $files = @("binary.exe")
 $boundary = [System.Guid]::NewGuid().ToString()
-$body = Get-MultipartBody $boundary $files $fields
+$tempFile = Get-MultipartBody $boundary $files $fields
 
 Write-Host "File.io Upload"
-$result = Invoke-RestMethod -Uri "https://file.io" -Method Post -Body $body -ContentType "multipart/form-data; boundary=$boundary" -TimeoutSec 300000 -MaximumRedirection 0
+$result = Invoke-RestMethod -Uri "https://file.io" -Method Post -ContentType "multipart/form-data; boundary=$boundary" -InFile $tempFile
 Write-Host "File is at https://file.io/$($result.key)"
